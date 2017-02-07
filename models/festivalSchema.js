0566157c7ddb7d7a49ba4b5b6bf11f69fdceccc0
Jmousy <joohe25@naver.ocm>
2017-02-07 11:39:42 +0900

    step4 에 필요한 파일 추가 second

---
+++ models/festivalSchema.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Jmousy on 2017. 2. 6..
+ */
+
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var festivalSchema = new Schema({
+    "행사명": String,
+    "장소": String,
+    "행사내용": String,
+    "행사시작일자": String,
+    "행사종료일자": String,
+    "행사시간": String,
+    "요금정보": String,
+    "주관기관": String,
+    "주최기관": String,
+    "연락처": String,
+    "후원기관": String,
+    "객석수": { type: Number, default: 0 },
+    "관람요금": String,
+    "입장연령": String,
+    "할인정보": String,
+    "유의사항": String,
+    "홈페이지주소": String,
+    "예매정보": String,
+    "주차장보유여부": { type: String, default: "N" },
+    "소재지도로명주소": String,
+    "소재지지번주소": String,
+    "경도(WGS84좌표)": String,
+    "위도(WGS84좌표)": String,
+    "데이터기준일자": String
+
+});
+
+var festival = mongoose.model('festival', festivalSchema);
+
+module.exports = festival;
---
+++ routes/festival.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Jmousy on 2017. 2. 6..
+ */
+
+var express = require('express');
+var router = express.Router();
+var festivalSchema = require('../models/festivalSchema');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    festivalSchema.find({}, function (err, festivalData) {
+        console.log("festival view engine 연결 라우터 접근 ");
+        res.render('festivalView');
+        });
+    }
+);
+router.get('/info', function (req, res, next) {
+        festivalSchema.find({}, function (err, festivalData) {
+            console.log("festival data 가져오는 라우터 접근");
+            res.json(festivalData);
+            console.log("festival data 가져오는 라우터 접근");
+        });
+    }
+);
+
+module.exports = router;
+
