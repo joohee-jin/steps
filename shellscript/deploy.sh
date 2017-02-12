1cb4ecb6d992fe916a5daf9cff6a698e300542ea
진주희 <jjh_work@naver.com>
2017-02-13 01:43:43 +0900

    step4가 서비스서버에도 동작하도록 셋팅

--- shellscript/deploy.sh
+++ shellscript/deploy.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-ssh irteamsu@10.110.241.148 'rm -rf ~/deploy'
+ssh irteamsu@10.110.241.148 'rm -rf ~/deploy_steps'
 ssh irteamsu@10.110.241.148 'sudo npm install pm2 -g'
 
 echo "-------------------------------------------send the directory build"
 rsync -av -e ssh ~/steps/build irteamsu@10.110.241.148:/home1/irteamsu
+ssh irteamsu@10.110.241.148 'cd ~'
+ssh irteamsu@10.110.241.148 'mv build deploy_steps'
 
 echo "-------------------------------------------restart server"
 ssh irteamsu@10.110.241.148 'sudo NODE_ENV=distribution pm2 restart server'
@@ -16,6 +18,6 @@ ssh irteamsu@10.110.243.32 'pwd'
 ssh irteamsu@10.110.243.32 'mongoimport --db test --collection festivals --type csv --headerline --file ./festival.csv --upsert --upsertFields=행사명,장소'
 ssh irteamsu@10.110.243.32 'rm festival.csv'
 
-echo "-------------------------------------------delete the directory of build"
-rm -rf ~/steps/build
+#echo "-------------------------------------------delete the directory of build"
+#rm -rf ~/steps/build
 echo "-------------------------------------------end of deploy"
