d13551c194cd71f970700a55908e6feb19869458
진주희 <jjh_work@naver.com>
2017-02-13 01:51:24 +0900

    step4가 서비스서버에도 동작하도록 셋팅2

--- shellscript/deploy.sh
+++ shellscript/deploy.sh
@@ -3,15 +3,21 @@
 ssh irteamsu@10.110.241.148 'rm -rf ~/deploy_steps'
 ssh irteamsu@10.110.241.148 'sudo npm install pm2 -g'
 
-echo "-------------------------------------------send the directory build"
+echo "-------------------------------------------Deploy the directory, build"
+echo "-------------------------------------------to Web Service Server 1"
 rsync -av -e ssh ~/steps/build irteamsu@10.110.241.148:/home1/irteamsu
 ssh irteamsu@10.110.241.148 'cd ~'
 ssh irteamsu@10.110.241.148 'mv build deploy_steps'
 
-echo "-------------------------------------------restart server"
+echo "-------------------------------------------to Web Service Server 2"
+rsync -av -e ssh ~/steps/build irteamsu@10.110.243.28:/home1/irteamsu
+ssh irteamsu@10.110.243.28 'cd ~'
+ssh irteamsu@10.110.243.28 'mv build deploy_steps'
+
+echo "-------------------------------------------Restart Service Server"
 ssh irteamsu@10.110.241.148 'sudo NODE_ENV=distribution pm2 restart server'
 
-echo "-------------------------------------------send the data"
+echo "-------------------------------------------Deploy the data to DB Server"
 rsync -av -e ssh ~/steps/festival.csv irteamsu@10.110.243.32:/home1/irteamsu
 ssh irteamsu@10.110.243.32 'cd ~ '
 ssh irteamsu@10.110.243.32 'pwd'
@@ -20,4 +26,4 @@ ssh irteamsu@10.110.243.32 'rm festival.csv'
 
 #echo "-------------------------------------------delete the directory of build"
 #rm -rf ~/steps/build
-echo "-------------------------------------------end of deploy"
+echo "-------------------------------------------End of Deploy"
